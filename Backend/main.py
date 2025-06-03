a456b9f3ef262421373e56c260206bb2f42c3ed5
sam <samanthamillows@gmail.com>
2025-06-03 02:46:41 +0200

    Include the New Route for similarity in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import releases, users, collections
+from app.api import releases, users, collections, similarity
 
 
 # Create the app instance
@@ -19,6 +19,7 @@ app.add_middleware(
 app.include_router(users.router, prefix="/api", tags=["users"])
 app.include_router(collections.router, prefix="/api", tags=["collections"])
 app.include_router(releases.router, prefix="/api", tags=["releases"])
+app.include_router(similarity.router, prefix="/api", tags=["similarity"])
 
 @app.get("/")
 async def root():
